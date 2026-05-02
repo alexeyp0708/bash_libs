e7c5c300376e4242d86c1a5a21c79b7abd751df0
Alexey Pakhomov <dev@sun66.ru>
2026-05-02 20:19:19 +0500

    sharevar fixed bug - escape sequences

--- share_var/shareVar.sh
+++ share_var/shareVar.sh
@@ -13,26 +13,27 @@ shareVar.export(){
 #If a variable exists, it will override its value. Those. can override local variables.
 # If the variable does not exist, will create a global variable
 #Warn: The user must take care to match the types of existing variables
-shareVar.import(){ 
+shareVar.import() {
     local vars="$@"
-    local import_vars=$(cat "$_share_var_space")
-    >"$_share_var_space"
+    ##This behavior is necessary when you need to free up space immediately. But why?
+    #local import_vars=$(cat "$_share_var_space")
+    #>"$_share_var_space"
     local import_var import_var_name
     local to_source=
     while IFS= read -r line; do
-        import_var="$(echo -e "$line"|sed  -r 's/^declare (-.)+ //')"
-        import_var_name="$(echo -e $import_var|grep -oP '^.+?(?==)')"
-        if [[ -z "$vars" || ! -z "$(echo $vars|grep -oP "(?:^| )$import_var(?:$| )")" ]]
-        then
-            if [ -v "$import_var_name" ]
-            then
+        import_var="$(echo "$line" | sed -r 's/^declare (-.)+ //')"
+        import_var_name="$(echo "$import_var" | grep -oP '^.+?(?==)')"
+        if [[ -z "$vars" || ! -z "$(echo "$vars" | grep -oP "(?:^| )$import_var(?:$| )")" ]]; then
+            if [ -v "$import_var_name" ]; then
                 to_source="$to_source\n$import_var"
-            else 
-                to_source="$to_source\n$(echo $line|sed -r 's/^declare /declare -g /')"      
+            else
+                to_source="$to_source\n$(echo $line | sed -r 's/^declare /declare -g /')"
             fi
-        fi        
-    done <<< "$import_vars" 
-    source <(echo -e "$to_source")  
+        fi
+    done <"$_share_var_space"  #<<<"$import_vars"
+    >"$_share_var_space"
+    to_source="$(echo "$to_source" | sed 's/\\n/\n/g')"
+    source <(echo "$to_source")
 }
 
 #Initializes space (file) for variables
